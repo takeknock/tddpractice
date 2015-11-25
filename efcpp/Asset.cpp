63ccacccce0c767fc28f7d9df12f1c7de1c7253b
mimasu <takeknock@gmail.com>
2015-11-26 02:03:28 +0900

    add const for class member functions

--- efcpp/Asset.cpp
+++ efcpp/Asset.cpp
@@ -12,29 +12,31 @@ Asset::~Asset()
 
 }
 
-const double Asset::calculateDrift(const double interestRate, const double volatility)
+const double Asset::calculateDrift(
+    const double interestRate, const double volatility) const
 {
     return interestRate - 0.5 * pow(volatility, 2);
 }
 
-const double Asset::calculateDt(const double maturity, const double numberOfSteps)
+const double Asset::calculateDt(
+    const double maturity, const double numberOfSteps) const
 {
     return maturity / numberOfSteps;
 }
 
 const double Asset::calculateChangeOfAsset(
-        const double volatility, const double dt, const double drift) 
+        const double volatility, const double dt, const double drift) const
 {
     return sqrt(pow(volatility, 2)  * dt + pow(drift * dt, 2));
 }
 
 const double Asset::calculateUpProbability(
-        const double drift, const double dt, const double changeOfAsset)
+        const double drift, const double dt, const double changeOfAsset) const
 {
     return 0.5 + 0.5 * drift * dt / changeOfAsset;
 }
 
-const double Asset::calculateDownProbability(const double upProbability)
+const double Asset::calculateDownProbability(const double upProbability) const
 {
     return 1.0 - upProbability;
 }
@@ -42,7 +44,7 @@ const double Asset::calculateDownProbability(const double upProbability)
 const double Asset::calculateLowestAssetAtMaturity(
         const double changeOfAsset, 
         const double initialAsset, 
-        const double numberOfSteps)
+        const double numberOfSteps) const
 {
     const double lowestAssetAtMaturity = 
         initialAsset * exp(- changeOfAsset * numberOfSteps);
--- efcpp/Asset.h
+++ efcpp/Asset.h
@@ -5,24 +5,23 @@ class Asset {
 public:
     Asset();
     ~Asset();
-    const double calculateDrift(const double interestRate, const double volatility);
+    const double calculateDrift(
+        const double interestRate, const double volatility) const;
 
-
-    const double calculateDt(const double maturity, const double numberOfSteps);
-
+    const double calculateDt(
+        const double maturity, const double numberOfSteps) const;
 
     const double calculateChangeOfAsset(
-        const double volatility, const double dt, const double drift); 
-
+        const double volatility, const double dt, const double drift) const; 
 
     const double calculateUpProbability(
-        const double drift, const double dt, const double changeOfAsset);
+        const double drift, const double dt, const double changeOfAsset) const;
 
-    const double calculateDownProbability(const double upProbability);
+    const double calculateDownProbability(const double upProbability) const;
 
     const double calculateLowestAssetAtMaturity(
         const double changeOfAsset, 
         const double initialAsset, 
-        const double numberOfSteps);
+        const double numberOfSteps) const;
     
 };
