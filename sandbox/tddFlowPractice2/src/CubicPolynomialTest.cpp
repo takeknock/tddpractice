3b386097f0901d4eca9994ed453c5be56b86d50a
mimasu <takeknock@gmail.com>
2016-01-14 21:57:59 +0900

    implement testOperatorImpl in CubicPolynomialTest

--- sandbox/tddFlowPractice2/src/CubicPolynomialTest.cpp
+++ sandbox/tddFlowPractice2/src/CubicPolynomialTest.cpp
@@ -20,14 +20,23 @@ namespace pln {
         CPPUNIT_ASSERT_DOUBLES_EQUAL(b, fc._b, 10e-8);
         CPPUNIT_ASSERT_DOUBLES_EQUAL(c, fc._c, 10e-8);
         CPPUNIT_ASSERT_DOUBLES_EQUAL(d, fc._d, 10e-8);
-        
-        
     }
     
     void CubicPolynomialTest::testOperatorImpl()
     {
-    
+        const double a = 1.0;
+        const double b = 2.0;
+        const double c = 3.0;
+        const double d = 4.0;
+
+        pln::CubicPolynomial fc(a, b, c, d);
+        const double x = 20.0;
+        const double y = fc.operatorImpl(x);
+        const double expected = a * x * x * x + b * x * x + c * x + d;       
+
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(expected, y, 10e-8);
     }
+
     
     void CubicPolynomialTest::testGetDiscriminantImpl()
     {
--- sandbox/tddFlowPractice2/src/main.cpp
+++ sandbox/tddFlowPractice2/src/main.cpp
@@ -1,3 +1,10 @@
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+
 #include <iostream>
 #include <boost/shared_ptr.hpp>
 
@@ -9,6 +16,23 @@
 
 int main()
 {
+    // for unit tests
+    CPPUNIT_NS::TestResult controller;
+
+    CPPUNIT_NS::TestResultCollector result;
+    controller.addListener(&result);
+
+    CPPUNIT_NS::BriefTestProgressListener progress;
+    controller.addListener(&progress);
+
+    CPPUNIT_NS::TestRunner runner;
+    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+    runner.run(controller);
+
+    CPPUNIT_NS::CompilerOutputter outputter(&result, CPPUNIT_NS::stdCOut());
+    outputter.write();
+
+
     // 1. use pointer
     boost::shared_ptr<pln::IPolynomial> pf(new pln::CubicPolynomial(1.0, 1.0, 2.0, 3.0));
     std::cout << (*pf)(1.0) << std::endl;
